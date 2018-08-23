62f11bdfec3e17d6e34fe6914fd6f5fb61a6d2a4
ldowns99 <42652512+ldowns99@users.noreply.github.com>
2018-08-23 13:43:34 -0500

    Update hello.c
    done

--- hello.c
+++ hello.c
@@ -1,5 +1,5 @@
 /**
- * Author: Chris Bourke
+ * Author: Chris Bourke, Luke Downs
  * Date: 2016/11/02
  *
  * A simple hello world program in C
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Leo, Computer Engineering\n");
+  printf("Leo, Computer Engineering, Luke Computer Science\n");
 
   return 0;
 }
